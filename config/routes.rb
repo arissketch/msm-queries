ad883ad22efa9552947fdb9aea823d3d4602eacf
arissketch <60331491+arissketch@users.noreply.github.com>
2024-05-02 05:48:08 +0000

    directors

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/directors",{:controller => "directors", :action => "index"})
+  get("/youngest",{:controller => "", :action => ""})
+  get("/eldest",{:controller => "", :action => ""})
+  get("/movies",{:controller => "", :action => ""})
+  get("/actors",{:controller => "", :action => ""})
+
+
+
+
 end
